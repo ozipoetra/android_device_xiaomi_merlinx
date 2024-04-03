bf6f65944480fbc721b586799fb229c8fa52a42a
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-04-03 06:41:24 +0000

    merlinx: Build mini gapps

--- evolution_merlinx.mk
+++ evolution_merlinx.mk
@@ -14,15 +14,49 @@ $(call inherit-product, device/xiaomi/merlinx/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
+# Debugging Flags
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := false
+
+# Evolution X flags
+EVO_BUILD_TYPE := COMMUNITY
+TARGET_BUILD_VIMUSIC := true
+TARGET_USES_MINI_GAPPS := true
+
+# Another stuff
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
+
+# Pixel customization
+TARGET_USE_PIXEL_FRAMEWORK := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
 PRODUCT_NAME := evolution_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9
 
+PRODUCT_SYSTEM_NAME := merlin
+PRODUCT_SYSTEM_DEVICE := merlin
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys"
+PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
